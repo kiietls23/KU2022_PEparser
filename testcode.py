f9df633f103c20716f947f221b87030cbd34ddce
sunuukim <63042552+sunuukim@users.noreply.github.com>
2022-11-09 20:01:57 +0900

    2022-11-09

--- testcode.py
+++ testcode.py
@@ -2,7 +2,7 @@ import pefile
 import struct
 import datetime
 
-path = 'C:\\Users\\USER\\OneDrive - 고려대학교\\study\\2022-1\\역공학\\CRACKME1\\CRACKME1.EXE'
+path = ''#분석할 파일 경로 입력
 pe = pefile.PE(path)
 print("파싱 시작 ...")
 # DOS HEADER
